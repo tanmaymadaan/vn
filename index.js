7a92da95df74f55e3442bddf201e50a07b7065c5
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-10 12:14:48 +0530

    mongoose, body-parser installed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/api', function(req, res){
-  console.log('GET Request');
-  res.send({
-    name: 'Tanmay'
-  });
-});
+mongoose.connect('mongodb://localhost/vn', { useNewUrlParser: true});
+
+app.use('/api', require('./routes/add-patient'));
+app.use(bodyParser.json());
 
 app.listen(process.env.port || 4000, function(){
   console.log('now listening for requests');
---
+++ routes/add-patient.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const Patient = require('../models/patient');
+
+router.post('/add-patient', function(req, res, next){
+  Patient.create(req.body).then(function(asset)){
+    res.send(asset);
+  }).catch(next);
+});
+
+module.exports = router;
