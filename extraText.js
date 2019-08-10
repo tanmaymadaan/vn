d1a23e3c6f6a8e2556a9e29280d70c323dc469b0
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-10 12:20:48 +0530

    initial mongo test, patient post

---
+++ extraText.js
@@ -0,0 +1,93 @@
+guardianName:{
+  type: String
+},
+idType:{
+  type: String
+},
+idNumber:{
+  type: Number
+},
+area:{
+  type: String
+},
+city:{
+  type: String
+},
+postalCode:{
+  type: Number
+},
+state:{
+  type: String
+},
+country:{
+  type: String
+},
+mobNo:{
+  type: Number
+},
+age:{
+  type: Number
+},
+gender:{
+  type: String
+},
+admDate:{
+  type: Date
+},
+surgDate:{
+  type: Date
+},
+disDate:{
+  type: Date
+},
+comor:{
+  type: String
+},
+surgeon:{
+  type: String
+},
+diagnosis:{
+  type: String
+},
+procedure:{
+  type: String
+},
+lateralType:{
+  type: String
+},
+eyeOp:{
+  type: String
+},
+preOpR:{
+  type: String
+},
+preOpL:{
+  type: String
+},
+postOpR:{
+  type: String
+},
+postOpL:{
+  type: String
+},
+lensNo:{
+  type: Number
+},
+preOpPhoto:{
+  type: Boolean
+},
+postOpPhoto:{
+  type: Boolean
+},
+fileComplete:{
+  type: Boolean
+},
+docSign:{
+  type: Boolean
+},
+disCert:{
+  type: Boolean
+},
+remark:{
+  type: String
+}
