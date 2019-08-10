0f07f355b2890ca4f2971c39f6a559915737057c
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-10 09:52:49 +0530

    index.js, testing response

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(){
+app.get('/api', function(req, res){
   console.log('GET Request');
+  res.send({
+    name: 'Tanmay'
+  });
 });
 
-app.listen(process.env.port, function(){
+app.listen(process.env.port || 4000, function(){
   console.log('now listening for requests');
 });
