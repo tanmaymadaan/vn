5e6ef4da2ab5a4c75a2a88d2a4b12935b9a298f7
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-10 09:43:08 +0530

    index.js, testing get request

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function(){
+  console.log('GET Request');
+});
+
 app.listen(process.env.port, function(){
   console.log('now listening for requests');
 });
