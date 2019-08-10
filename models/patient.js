ff1104746f3ba50660cb896def6a0745713ddb4c
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-10 12:00:30 +0530

    patient model created

---
+++ models/patient.js
@@ -0,0 +1,107 @@
+ const mongoose = require('mongoose');
+ const Schema = mongoose.Schema;
+
+ const PatientSchema = new Schema({
+   mrdNo:{
+     type: String
+   },
+   firstName:{
+     type: String
+   },
+   lastName:{
+     type: String
+   },
+   guardianName:{
+     type: String
+   },
+   idType:{
+     type: String
+   },
+   idNumber:{
+     type: Number
+   },
+   area:{
+     type: String
+   },
+   city:{
+     type: String
+   },
+   postalCode:{
+     type: Number
+   },
+   state:{
+     type: String
+   },
+   country:{
+     type: String
+   },
+   mobNo:{
+     type: Number
+   },
+   age:{
+     type: Number
+   },
+   gender:{
+     type: String
+   },
+   admDate:{
+     type: Date
+   },
+   surgDate:{
+     type: Date
+   },
+   disDate:{
+     type: Date
+   },
+   comor:{
+     type: String
+   },
+   surgeon:{
+     type: String
+   },
+   diagnosis:{
+     type: String
+   },
+   procedure:{
+     type: String
+   },
+   lateralType:{
+     type: String
+   },
+   eyeOp:{
+     type: String
+   },
+   preOpR:{
+     type: String
+   },
+   preOpL:{
+     type: String
+   },
+   postOpR:{
+     type: String
+   },
+   postOpL:{
+     type: String
+   },
+   lensNo:{
+     type: Number
+   },
+   preOpPhoto:{
+     type: Boolean
+   },
+   postOpPhoto:{
+     type: Boolean
+   },
+   fileComplete:{
+     type: Boolean
+   },
+   docSign:{
+     type: Boolean
+   },
+   disCert:{
+     type: Boolean
+   },
+   remark:{
+     type: String
+   }
+ });
