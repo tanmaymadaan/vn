d86eaf175954c2f78a77cae098a4e999f7d462b1
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-15 14:07:34 +0530

    creating counter collection

--- routes/add-patient.js
+++ routes/add-patient.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const Patient = require('../models/patient');
+const Counter = require('../models/counter');
 
 router.post('/add-patient', function(req, res, next){
     Patient.create(req.body).then(function(patient){
-      console.log('patient added', req.body.);
+      console.log('patient added', req.body.firstName);
       res.send(patient);
     }).catch(next);
 });
--- routes/get-counter.js
+++ routes/get-counter.js
@@ -3,10 +3,17 @@ const router = express.Router();
 
 const Counter = require('../models/counter');
 
+router.post('/get-counter', function(req, res, next){
+    Counter.create(req.body).then(function(patient){
+      res.send(patient);
+    }).catch(next);
+});
+
+/*
 router.get('/get-counter', function(req, res, next){
-  Counter.find({val: 'counterId'}.then(function(counter){
+  Counter.find({val: 'counterId'}).then(function(counter){
     res.send(counter);
   }).catch(next);
-});
+});*/
 
 module.exports = router;
