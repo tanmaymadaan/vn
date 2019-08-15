6573c4b64ed04aea3448d09eac44109522666dc8
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-15 14:09:57 +0530

    fetching counter

--- routes/get-counter.js
+++ routes/get-counter.js
@@ -3,17 +3,10 @@ const router = express.Router();
 
 const Counter = require('../models/counter');
 
-router.post('/get-counter', function(req, res, next){
-    Counter.create(req.body).then(function(patient){
-      res.send(patient);
-    }).catch(next);
-});
-
-/*
 router.get('/get-counter', function(req, res, next){
   Counter.find({val: 'counterId'}).then(function(counter){
     res.send(counter);
   }).catch(next);
-});*/
+});
 
 module.exports = router;
