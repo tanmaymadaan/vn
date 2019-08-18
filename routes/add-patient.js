8fa9c5ce74e6d2e3e6e666e374cbf05954ffbddb
Tanmay Madaan <tanmaymadaan@Tanmays-MacBook-Pro.local>
2019-08-18 10:36:14 +0530

    counter update

--- routes/add-patient.js
+++ routes/add-patient.js
@@ -5,7 +5,15 @@ const Patient = require('../models/patient');
 const Counter = require('../models/counter');
 
 router.post('/add-patient', function(req, res, next){
+
     Patient.create(req.body).then(function(patient){
+      Counter.findByIdAndUpdate({_id: '5d5516f6a99a93a312d8a59b'}, {$inc: {counter: 1}}, function(err, Counter){
+          if(err)
+            return next(err);
+          console.log('counter updated');
+        }
+      );
+
       console.log('patient added', req.body.firstName);
       res.send(patient);
     }).catch(next);
